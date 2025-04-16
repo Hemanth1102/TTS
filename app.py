9c2ffca62fe9cfb7eb92d1e7eb8745115b19dcf2
SaIHeMaNtH <154537128+Hemanth1102@users.noreply.github.com>
2025-04-16 14:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def synthesize():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
